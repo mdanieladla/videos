0fe313e50c5ea9b83e422ddecf763e1ad3c766a8
mdanieladla <mariadanieladarnea@gmail.com>
2022-06-25 14:57:48 +0200

    added axios fetch, added components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  handleTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
+  handleVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
   render() {
     return (
       <div className='ui container'>
-        <SearchBar />
+        <SearchBar onFormSubmit={this.handleTermSubmit} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.handleVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }

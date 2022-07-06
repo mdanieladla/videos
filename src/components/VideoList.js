abcd012ea8fa40a2e13e8a0b69a876a23dd407db
mdanieladla <mariadanieladarnea@gmail.com>
2022-07-06 18:25:34 +0200

    added classname div temporal

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -12,7 +12,9 @@ const VideoList = ({ videos, onVideoSelect }) => {
     );
   });
 
-  return <div className='ui relaxed divided list'>{renderedList}</div>;
+  return (
+    <div className='ui relaxed divided list each-video'>{renderedList}</div>
+  );
 };
 
 export default VideoList;
